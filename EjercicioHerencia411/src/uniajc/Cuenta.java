2c72bea0496e854a2f919d14aee90c26d5fe5296
Gabriel Perez <gperezm@profesores.uniajc.edu.co>
2025-03-25 19:01:00 -0500

    Creacion de la clase cuenta con sus atributos

---
+++ EjercicioHerencia411/src/uniajc/Cuenta.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package uniajc;
+
+/**
+ *
+ * @author g.perezmoreno
+ */
+public class Cuenta {
+    protected float saldo;
+    protected int numeroConsignaciones = 0;
+    protected int numeroRetiros = 0;
+    protected float tasaAnual;
+    protected float comisionMensual = 0;
+    
+}
